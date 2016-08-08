996fa80231186fb6c0cfe42f64aac1fd17ad7aae
shak-mar <shakmar@openmailbox.org>
2016-08-08 17:38:52 +0200

    Move cucinoMappings into cucino Main in order to resemble README.md snippet

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,27 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
-import Testing.Cucumber.Main (cucinoMappings, cucumber)
+import Data.Functor.Identity (Identity)
+import Data.Monoid ((<>))
+import Testing.Cucumber.Main (cucumber)
+import Testing.Cucumber.Types (Mapping(..), Report, mapp)
+import Text.Read (readMaybe)
 
 main = cucumber cucinoMappings
+
+cucinoMappings :: [Mapping Identity]
+cucinoMappings =
+  [ mapp Given "^a calculator$" $ \[] () () -> Right ()
+  , mapp When  "^these numbers are summed together:$" $ \[] (s :: String) () ->
+      maybe (Left "Should be one number per line") Right $
+        sum <$> traverse readMaybe (lines s) :: Report Int
+  , mapp When  "^these numbers are multiplied:$" $ \[] (s :: [[String]]) () ->
+      maybe (Left "Should be one number per cell") Right $
+        foldl (*) 1 <$> traverse readMaybe (concat s) :: Report Int
+  , mapp Then  "^the result is ([0-9]+)$" $ \[x] () n ->
+      case (n ==) <$> (readMaybe x :: Maybe Int) of
+        Just True  -> Right ()
+        Just False -> Left $ "Expected " <> show n <> " to be " <> x
+        Nothing    -> Left "Not a number!"
+  ]
--- src/Testing/Cucumber/Main.hs
+++ src/Testing/Cucumber/Main.hs
@@ -109,19 +109,3 @@ cucumberOnScen ms s = return . ScenarioResult s . reverse . fst $
         matches :: Step -> [(Dynamic -> Report Dynamic)]
         matches s = catMaybes $ map (($ stepMultiline s) . ($ stepText s) .
                                      ($ stepKeyword s) . match) ms
-
-cucinoMappings :: [Mapping Identity]
-cucinoMappings =
-  [ mapp Given "^a calculator$" $ \[] () () -> Right ()
-  , mapp When  "^these numbers are summed together:$" $ \[] (s :: String) () ->
-      maybe (Left "Should be one number per line") Right $
-        sum <$> traverse readMaybe (lines s) :: Report Int
-  , mapp When  "^these numbers are multiplied:$" $ \[] (s :: [[String]]) () ->
-      maybe (Left "Should be one number per cell") Right $
-        foldl (*) 1 <$> traverse readMaybe (concat s) :: Report Int
-  , mapp Then  "^the result is ([0-9]+)$" $ \[x] () n ->
-      case (n ==) <$> (readMaybe x :: Maybe Int) of
-        Just True  -> Right ()
-        Just False -> Left $ "Expected " <> show n <> " to be " <> x
-        Nothing    -> Left "Not a number!"
-  ]
