9df448bd8ae162e02f7c85e792b9aeb6cbd02bac
shak-mar <shakmar@openmailbox.org>
2016-08-08 18:04:23 +0200

    Update default.nix with text dependency

--- default.nix
+++ default.nix
@@ -4,15 +4,16 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { cabal-install, mkDerivation, abacate, base, mtl, regex-posix, stdenv }:
+  f = { cabal-install, mkDerivation, abacate, base, mtl, regex-posix, stdenv, text
+      }:
       mkDerivation {
         pname = "cucumber";
         version = "0.1.0.0";
         src = ./.;
         isLibrary = true;
         isExecutable = true;
-        libraryHaskellDepends = [ cabal-install abacate base mtl regex-posix ];
-        executableHaskellDepends = [ cabal-install base ];
+        libraryHaskellDepends = [ cabal-install abacate base mtl regex-posix text ];
+        executableHaskellDepends = [ cabal-install abacate base mtl regex-posix text ];
         license = stdenv.lib.licenses.bsd3;
       };
 
